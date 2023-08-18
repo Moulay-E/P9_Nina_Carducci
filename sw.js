7cfb34e2cbc66ddafec3c933247abb5a23bee5f3
Moulay-E <Moulay553E@gmail.com>
2023-08-18 21:13:56 +0200

    add img to cache

--- sw.js
+++ sw.js
@@ -2,9 +2,21 @@ const cacheName = 'my-cache-v1';
 const assetsToCache = [
   "./assets/images/instagram.png",
   "./assets/images/slider/ryoji-iwata-wUZjnOv7t0g-unsplashTiny_Pc.avif",
+  "./assets/images/slider/ryoji-iwata-wUZjnOv7t0g-unsplash.avif",
   "./assets/images/slider/nicholas-green-nPz8akkUmDI-unsplashTiny_Pc.avif",
+  "./assets/images/slider/nicholas-green-nPz8akkUmDI-unsplash.avif",
   "./assets/images/slider/edward-cisneros-3_h6-1NPDGw-unsplashTiny_Pc.avif",
+  "./assets/images/slider/edward-cisneros-3_h6-1NPDGw-unsplash.avif",
   './assets/images/nina.avif',
+  "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.avif",
+  "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.avif",
+  "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.avif",
+  "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.avif",
+  "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.avif",
+  "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.avif",
+  "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.avif",
+  "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.avif",
+  "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.avif",
   // ... ajoutez toutes les autres ressources que vous souhaitez mettre en cache
 ];
 
